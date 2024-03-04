7c4bcd05e7f0d716a54cd27dcbd5f65573c505a7
optimus9811 <vikasyadav5073@gmail.com>
2024-03-04 06:24:58 +0000

    gs201:Bringup @crdroidandroid Signed-off-by: optimus9811 <vikasyadav5073@gmail.com>

--- lineage_common.mk
+++ lineage_common.mk
@@ -15,17 +15,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
     ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
-
-# GMS
-ifeq ($(WITH_GMS),true)
-GMS_MAKEFILE=gms_64bit_only.mk
-endif
-
+	
 # Google Assistant
 PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
-
-# Lineage Health
-include hardware/google/pixel/lineage_health/device.mk
 
 # Linker config
 PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
@@ -35,5 +27,3 @@ PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
 PRODUCT_PACKAGES += \
     GoogleParts
 
-# Touch
-include hardware/google/pixel/touch/device.mk
